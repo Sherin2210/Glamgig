29dfb628a490abbec9c89b9fdf0575b36d401436
Sherin <sherinbabu2995@gmail.com>
2023-12-03 10:52:32 -0800

    Initial commit

---
+++ app/src/main/java/com/se/glamgig/MenActivity.java
@@ -0,0 +1,16 @@
+package com.se.glamgig;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.se.glamgig.R;
+
+public class MenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_men);
+    }
+}
